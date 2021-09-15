8d10d35b61f3d5be159d371e06322c935cf5afdf
AleksanderSzok <43827574+AleksanderSzok@users.noreply.github.com>
2021-09-15 21:40:19 +0200

    Update k_means_alg.py

--- k_means_alg.py
+++ k_means_alg.py
@@ -14,7 +14,20 @@ class PointsMap:
     def bind_created_map(self):
         self.map_point = self.create_map()
 
-    def choose_centroids(self):
+    def choose_centroids(self, k_value=3):
         if self.map_point is not None:
-            random_index = np.random.choice(self.size, size=3, replace=False)
+            random_index = np.random.choice(self.size, size=k_value, replace=False)
             return self.map_point[random_index,:]
+
+
+class KZones:
+    def __init__(self, lenght=100, size=20, k_value=3):
+        self.lenght = lenght
+        self.size = size
+        self.k_value = k_value
+        self.points_set, self.centroids = self.initialize_points_set()
+
+    def initialize_points_set(self):
+        points_map_instance = PointsMap(lenght=self.lenght, size=self.size)
+        points_map_instance.bind_created_map()
+        return points_map_instance.map_point, points_map_instance.choose_centroids(self.k_value)

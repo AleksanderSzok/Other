21b3dd4ef800dca8410a9a36d7a26cf4b7f0949d
AleksanderSzok <43827574+AleksanderSzok@users.noreply.github.com>
2021-09-19 22:54:17 +0200

    Update k_means_alg.py

--- k_means_alg.py
+++ k_means_alg.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 class PointsMap:
-    def __init__(self, lenght=100, width=100, size=20, map_point=None):
+    def __init__(self, lenght: int = 100, width: int = 100, size: int = 20, map_point=None):
         self.lenght = lenght
         self.width = width
         self.size = size
@@ -19,9 +19,10 @@ class PointsMap:
             random_index = np.random.choice(self.size, size=k_value, replace=False)
             return self.map_point[random_index,:]
 
-
+# PointsMap(map_point= ...)
+# PointsMap.choose_centroids()
 class KZones:
-    def __init__(self, lenght=100, size=20, k_value=3):
+    def __init__(self, lenght: int = 100, size: int = 20, k_value: int = 3):
         self.lenght = lenght
         self.size = size
         self.k_value = k_value
@@ -31,3 +32,20 @@ class KZones:
         points_map_instance = PointsMap(lenght=self.lenght, size=self.size)
         points_map_instance.bind_created_map()
         return points_map_instance.map_point, points_map_instance.choose_centroids(self.k_value)
+
+    def assign_point_to_zone(self):
+        assignment_list = []
+        for elem in self.points_set:
+            tmp_list = []
+            for centroid in self.centroids:
+                tmp_list.append(np.linalg.norm([elem - centroid]))
+            assignment_list.append(np.argmin(tmp_list))
+        return assignment_list
+
+    def new_centroids(self):
+        assignment_list = np.array(self.assign_point_to_zone()) # todo change to array in assign_point_to_zone method
+        new_centroid_list = np.empty((1,2), dtype=int)
+        for i in range(self.k_value):
+            tmp = self.points_set[assignment_list == i].mean(axis=0)
+            new_centroid_list = np.append(new_centroid_list, [tmp], axis=0)
+        return new_centroid_list[1:]

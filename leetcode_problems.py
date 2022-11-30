607e1fe475bd4ff5200dd443306d14851e793382
AleksanderSzok <szokmaster@gmail.com>
2022-11-30 23:25:28 +0100

    added  kruskalmstrsub solution

--- leetcode_problems.py
+++ leetcode_problems.py
@@ -27,3 +27,30 @@ def combination_sum(
 
 
 print(combinationSum([2, 3, 6, 7], 7))
+
+
+# HackerRank kruskalmstrsub problem:
+def kruskals(g_nodes, g_from, g_to, g_weight):
+    components = {i: {i} for i in range(1, g_nodes + 1)}
+    edges = list(zip(g_from, g_to, g_weight))
+    edges.sort(key=lambda i: i[2])
+    weights_of_edges = 0
+    lenght_of_component = 0
+    for edge in edges:
+        if lenght_of_component == g_nodes:
+            break
+        tmp = 0
+        for k in components.keys():
+            if edge[0] in components[k]:
+                tmp = k
+                break
+        for p in components.keys():
+            if edge[1] in components[p]:
+                if p != tmp:
+                    weights_of_edges += edge[2]
+                    components[tmp].update(components[p])
+                    lenght_of_component = len(components[tmp])
+                    del components[p]
+                    break
+
+    return weights_of_edges
